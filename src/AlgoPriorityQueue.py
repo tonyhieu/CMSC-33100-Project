21a21259d6bd5d8dd652f956ec02d28d99526748
dfuehne <duncan.fuehne@gmail.com>
2026-02-26 15:23:25 -0600

    priority queue implemented

--- src/AlgoPriorityQueue.py
+++ src/AlgoPriorityQueue.py
@@ -6,6 +6,7 @@ from .Job import Job
 import heapq
 import itertools
 from enum import Enum
+import numpy as np
 
 class PriorityType(Enum):
     expectedLength = "expectedLength"
@@ -18,6 +19,7 @@ class AlgoPriorityQueue(AlgoBase):
         self.nCores = nCores
         self.priorityType = priorityType
         self.jobQueue = [[] for _ in range(self.nCores)] #initialize list which heapq uses
+        self.queueExpectedDuration = [0.0 for _ in range(self.nCores)]
 
     def handleJobSubmission(self, job: Job):
 
@@ -41,78 +43,119 @@ class AlgoPriorityQueue(AlgoBase):
                 coreEndTime = self.currentSchedule.getExactEndTime(coreID)
         
         '''
-        we now "stop" the current segment, split the part that has not finished into
-        a smaller segment, then add both the running segment and the submitted job(conveerted to segment)
-        into the heap queue
+        we now add the current job's threads into the various priority queues, based on the 
+        total running time accumulated in each core's queues
         '''
+        threadPriorities = np.zeros(job.nThreads)
+        threadTieCounts = np.zeros(job.nThreads, dtype = int)
+        threadCores = np.zeros(job.nThreads, dtype = int)
+        for threadID, thread in enumerate(job.threads):
+            #check core with earliest expected start time
+            earliestCore = 0
+            earliestQueueTime = self.getQueueFinishTime(job.submissionTime, 0)
+            for coreID in range(1, self.nCores):
+                coreEarliestQueueTime = self.getQueueFinishTime(job.submissionTime, coreID)
+                if coreEarliestQueueTime < earliestQueueTime:
+                    earliestQueueTime = coreEarliestQueueTime
+                    earliestCore = coreID
 
-        #first check if job is currently running, trim and add not run part in queue
-        if job.submissionTime < self.currentSchedule.getExactEndTime():
-            runningSegment = self.currentSchedule.trimRunningSegment(job.submissionTime)
-            runningTime = job.submissionTime - runningSegment.startTime#time the segment got to run
-            #add the running segment as another job to the heap queue
-            runningJob = ScheduledJob(self.scheduledJobs[runningSegment.jobID])
+            if self.priorityType == PriorityType.expectedLength:
+                priority = thread.expectedLength
 
-            if (runningTime > runningJob.expectedLength):
-                expectedRemainingTime = 0.0
-            else:
-                expectedRemainingTime = runningJob.expectedLength - runningTime
-            heapq.heappush(self.jobQueue, (expectedRemainingTime, next(tieBreakingCounter), runningJob))
+            tieCount = next(tieBreakingCounter)
+            heapq.heappush(self.jobQueue[earliestCore], (priority, tieCount, thread))
+            self.queueExpectedDuration[earliestCore] += thread.expectedLength
+            threadPriorities[threadID] = priority
+            threadTieCounts[threadID] = tieCount
+            threadCores[threadID] = coreID
 
-        submittedJob = ScheduledJob(job)
-        submittedJobTieCount = next(tieBreakingCounter)
-        heapq.heappush(self.jobQueue, (submittedJob.expectedLength, submittedJobTieCount, submittedJob))
-        expectedFinishTime = job.submissionTime + self.getExpectedDurationUntilJob(submittedJob.id, submittedJob.expectedLength, submittedJobTieCount) + submittedJob.expectedLength
-        submittedJob.setExpectedFinishTime(expectedFinishTime)
-        self.scheduledJobs[job.id] = submittedJob
+        """
+        now that we added all of the threads to a queue, we need to calculate the 
+        expected end time for the job by finding maximum thread end time
+        """
+        maximumExpectedFinishTime = 0.0
+        for theadID, thread in enumerate(job.threads):
+            '''
+            the time the next element will be released plus 
+            the expected duration of all items with higher priority in the queue
+            plus the expected duration of the thread
+            '''
+            expectedFinishTime = self.getQueueReleaseTime(job.submissionTime, threadCores[theadID]) \
+                                    + self.getExpectedDurationUntilThread(threadCores[theadID], 
+                                                                     threadPriorities[theadID],
+                                                                     threadTieCounts[theadID]) \
+                                    + thread.expectedLength
+            maximumExpectedFinishTime = max(maximumExpectedFinishTime, expectedFinishTime)                   
+
+
+        scheduledJob = ScheduledJob(job)
+        scheduledJob.setExpectedFinishTime(maximumExpectedFinishTime)
+        self.scheduledJobs[job.id] = scheduledJob
 
     def scheduleThreadFromHeapQueue(self, coreID):
-        if len(self.jobQueue) == 0:
+        if len(self.jobQueue[coreID]) == 0:
             raise ValueError("No Jobs in Queue to Schedule")
 
-        priority, tieCount, jobToSchedule = heapq.heappop(self.jobQueue)
-        jobStartTime = max(self.currentSchedule.getExactEndTime(), 
-                           jobToSchedule.submissionTime)
-        jobEndTime = jobStartTime + jobToSchedule.intervalLength
+        priority, tieCount, threadToSchedule = heapq.heappop(self.jobQueue[coreID])
+        self.queueExpectedDuration[coreID] -= threadToSchedule.expectedLength
+        threadStartTime = max(self.currentSchedule.getExactEndTime(coreID), 
+                           threadToSchedule.submissionTime)
+        threadEndTime = threadStartTime + threadToSchedule.actualLength
 
-        segmentID = self.scheduledJobs[jobToSchedule.id].getNumberOfSegments()
-        segment = Segment(segmentID, jobStartTime, jobEndTime, jobToSchedule.id, jobToSchedule.expectedLength)
-        self.scheduledJobs[jobToSchedule.id].addSegment(segment)
+        segmentID = self.scheduledJobs[threadToSchedule.jobID].getNumberOfScheduledSegments()
+        segment = Segment(segmentID, 
+                          threadToSchedule.jobID, 
+                          coreID, 
+                          threadToSchedule.threadID, 
+                          threadStartTime, 
+                          threadEndTime, 
+                          threadToSchedule.expectedLength)
+        self.scheduledJobs[threadToSchedule.jobID].addSegment(segment)
         self.currentSchedule.addSegment(segment)
 
-    def getExpectedDurationUntilJob(self, jobID, jobPriority, jobTieCount):
+    def getQueueFinishTime(self, submissionTime, coreID):
+        '''
+        the expected finish time for the queue is the time the next 
+        element will be released
+        plus the expected length of the queue
+        '''
+        return self.getQueueReleaseTime(submissionTime, coreID) \
+                + self.queueExpectedDuration[coreID]
+    
+    def getQueueReleaseTime(self, submissionTime, coreID):
+        '''
+        the expected release time for the queue on a specific core is either the submission time, or the 
+        expected finish time of the last job, whichever is latest
+        '''
+        return max(submissionTime, self.currentSchedule.getLastJobsExpectedEndTime(coreID))
 
-        numJobsWithID = 0
+    def getExpectedDurationUntilThread(self, coreID, threadPriority, threadTieCount):
         expectedQueueDuration = 0.0
-        for priority, tieCount, job in self.jobQueue:         
-            print(priority)
-            print(jobPriority)
-            if jobID == job.id:
-                numJobsWithID += 1
-                if numJobsWithID > 1:
-                    raise ValueError("Multiple Jobs with same ID in heap Queue")
-            elif jobPriority == priority:
-                if tieCount < jobTieCount:
-                    expectedQueueDuration += job.expectedLength
-            elif priority < jobPriority:
-                expectedQueueDuration += job.expectedLength
+        """
+        go through queue, and add up the expected wait time of everything 
+        that will be released before it (lower priority)
+        """
+        for priority, tieCount, thread in self.jobQueue[coreID]:
+            if threadPriority == priority:
+                if tieCount < threadTieCount:
+                    expectedQueueDuration += thread.expectedLength
+            elif priority < threadPriority:
+                expectedQueueDuration += thread.expectedLength
 
         return expectedQueueDuration
                 
-
-        
-
 
     def evaluateSchedule(self):
         '''
         handleJobSubmission Placed Jobs in the queue, here we have to empty the 
         queue into the schedule before we analyze the schedule
         '''
-        while len(self.jobQueue) > 0:
-            self.scheduleJobFromHeapQueue()
+        for coreID in range(self.nCores):
+            while len(self.jobQueue[coreID]) > 0:
+                self.scheduleThreadFromHeapQueue(coreID)
 
         self.currentSchedule.dump()
-        sp = SchedulePerformance.SchedulePerformance(self.scheduledJobs)
+        sp = SchedulePerformance(self.scheduledJobs)
         
         print("\n\n")
         return sp
